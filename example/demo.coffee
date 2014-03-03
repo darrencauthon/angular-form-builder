c60f712a9ae4984862a18f0bab32eb903d087868
Darren Cauthon <darren@cauthon.com>
2014-03-02 19:06:26 -0600

    More samples.

--- example/demo.coffee
+++ example/demo.coffee
@@ -4,13 +4,22 @@ angular.module 'app', ['builder', 'builder.components', 'validator.rules']
     # ----------------------------------------
     # builder
     # ----------------------------------------
-    #textbox = $builder.addFormObject 'default',
-    #component: 'textInput'
-    #label: 'Name'
-    #description: 'Your name'
-    #placeholder: 'Your name'
-    #required: yes
-    #editable: no
+
+
+    data = [{"id":2,"component":"textInput","editable":true,"index":0,"label":"Text Input","description":"description","placeholder":"placeholder","options":[],"required":false,"validation":"/.*/"},
+            {"id":3,"component":"textArea","editable":true,"index":1,"label":"Text Area","description":"description","placeholder":"placeholder","options":[],"required":false,"validation":"/.*/"},
+            {"id":4,"component":"checkbox","editable":true,"index":2,"label":"Checkbox","description":"description","placeholder":"placeholder","options":["value one","value two"],"required":false,"validation":"/.*/"},
+            {"id":5,"component":"radio","editable":true,"index":3,"label":"Radio","description":"description","placeholder":"placeholder","options":["value one","value two"],"required":false,"validation":"/.*/"},
+            {"id":6,"component":"select","editable":true,"index":4,"label":"Select","description":"description","placeholder":"placeholder","options":["value one","value two"],"required":false,"validation":"/.*/"}]
+    for item in data
+      textbox = $builder.addFormObject 'default',
+        component: item.component
+        label: item.label
+        description: item.description
+        placeholder: item.placeholder
+        required: item.required
+        editable: yes
+        newRow: item.newRow
 
     # formObjects
     $scope.form = $builder.forms['default']
--- example/demo.js
+++ example/demo.js
@@ -1,5 +1,76 @@
 (function() {
   angular.module('app', ['builder', 'builder.components', 'validator.rules']).controller('DemoController', function($scope, $builder, $validator) {
+    var data, item, textbox, _i, _len;
+    data = [
+      {
+        "id": 2,
+        "component": "textInput",
+        "editable": true,
+        "index": 0,
+        "label": "Text Input",
+        "description": "description",
+        "placeholder": "placeholder",
+        "options": [],
+        "required": false,
+        "validation": "/.*/"
+      }, {
+        "id": 3,
+        "component": "textArea",
+        "editable": true,
+        "index": 1,
+        "label": "Text Area",
+        "description": "description",
+        "placeholder": "placeholder",
+        "options": [],
+        "required": false,
+        "validation": "/.*/"
+      }, {
+        "id": 4,
+        "component": "checkbox",
+        "editable": true,
+        "index": 2,
+        "label": "Checkbox",
+        "description": "description",
+        "placeholder": "placeholder",
+        "options": ["value one", "value two"],
+        "required": false,
+        "validation": "/.*/"
+      }, {
+        "id": 5,
+        "component": "radio",
+        "editable": true,
+        "index": 3,
+        "label": "Radio",
+        "description": "description",
+        "placeholder": "placeholder",
+        "options": ["value one", "value two"],
+        "required": false,
+        "validation": "/.*/"
+      }, {
+        "id": 6,
+        "component": "select",
+        "editable": true,
+        "index": 4,
+        "label": "Select",
+        "description": "description",
+        "placeholder": "placeholder",
+        "options": ["value one", "value two"],
+        "required": false,
+        "validation": "/.*/"
+      }
+    ];
+    for (_i = 0, _len = data.length; _i < _len; _i++) {
+      item = data[_i];
+      textbox = $builder.addFormObject('default', {
+        component: item.component,
+        label: item.label,
+        description: item.description,
+        placeholder: item.placeholder,
+        required: item.required,
+        editable: true,
+        newRow: item.newRow
+      });
+    }
     $scope.form = $builder.forms['default'];
     $scope.input = [];
     $scope.defaultValue = {};
